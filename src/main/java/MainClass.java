548c110b3dfbdc145e546eace50590efa14259d8
Augusto Cezar Ribeiro Santana <cezaodabahia@gmail.com>
2022-06-06 18:11:06 -0300

    Exemplos

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,13 +1,21 @@
 import context.Calculadora;
-import strategy.Resto;
-import strategy.Subtracao;
+import strategy.*;
 
 public class MainClass {
     public static void main(String[] args) {
-        Calculadora calculadora = new Calculadora(new Resto());
+        Calculadora calculadora = new Calculadora(new Soma());
         System.out.println(calculadora.calcula(10, 5));
 
         calculadora.setOperação(new Subtracao());
+        System.out.println(calculadora.calcula(10, 5));
+
+        calculadora.setOperação(new Multiplicacao());
+        System.out.println(calculadora.calcula(10, 5));
+
+        calculadora.setOperação(new Divisao());
+        System.out.println(calculadora.calcula(10, 5));
+
+        calculadora.setOperação(new Resto());
         System.out.println(calculadora.calcula(10, 5));
     }
 
