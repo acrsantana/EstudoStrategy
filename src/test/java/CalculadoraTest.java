11e768340c0008e802d277ab681b9341c4b84e16
Augusto Cezar Ribeiro Santana <cezaodabahia@gmail.com>
2022-06-06 18:56:55 -0300

    Adição de testes unitários

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import strategy.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class CalculadoraTest {
     private Calculadora calc = new Calculadora(new Soma());
@@ -10,10 +11,19 @@ public class CalculadoraTest {
     @Test
     void soma() {
         assertEquals(15, calc.calcula(10, 5));
+    }
+    @Test
+    void subtracao() {
         calc.setOperação(new Subtracao());
         assertEquals(5, calc.calcula(10, 5));
+    }
+    @Test
+    void multiplicacao() {
         calc.setOperação(new Multiplicacao());
         assertEquals(50, calc.calcula(10, 5));
+    }
+    @Test
+    void divisao() {
         calc.setOperação(new Divisao());
         assertEquals(2, calc.calcula(10, 5));
         calc.setOperação(new Resto());
